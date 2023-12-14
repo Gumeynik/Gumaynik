38779cddb35f17261e36f66e86701e777a5db0b7
Aleksandr Kubenko <151834458+Gumeynik@users.noreply.github.com>
2023-12-14 16:34:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.outout');
+
+button.addEventListener('click', function () {
+    output.innerText = 'Мне 25 лет';
+});
